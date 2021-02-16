ac9ceed1d7b08e3db30c2e3a18d6936a63c6d2ad
Luis Chavarriaga <luischa123@gmail.com>
2021-02-15 22:29:28 -0500

    fix compilation, stylistic changes
    este commit arregla la compilación de la aplicación.
    asimismo hace algunos cambios estéticos.

--- src/Server.hs
+++ src/Server.hs
@@ -16,48 +16,25 @@ import Control.Monad.IO.Class (MonadIO(liftIO))
 import Database.SQLite.Simple
 import Data.Text (Text)
 
--- lista temporal de usuarios
-users1 :: [User]
-users1 =
-  [ User {name = "Carlos", idn = 1, email = "carlos@logo.com",
-          registrationDay = fromGregorian 2010 2 3},
-    User {name = "Luis", idn = 2, email = "luis@logo.com",
-          registrationDay = fromGregorian 2010 5 6}
-  ]
-
--- lista temporal de películas
-movies1 :: [Movie]
-movies1 =
-  [ Movie {idn = 1, title = "Godzilla",
-           description = "king of the monsters",
-           duration = 120, rating = MayoresDe12},
-    Movie {idn = 2, title = "King Kong",
-           description = "eighth wonder of the world",
-           duration = 130, rating = MayoresDe12}
-  ]
-
-queryById :: (Z.HasField "idn" a Int) => [a] -> Maybe Int -> Handler a
-queryById _ Nothing = throwError err404 {errBody = "No se especificó ID"}
-queryById source (Just idd) = let item = filter (\x -> x.idn == idd) source
-  in if not $ null item
-  then return $ head item
-  else throwError err404 {errBody = "No existe"}
+-- --
 
 handleNewUser :: String -> User -> Handler String
 handleNewUser dbfile User{..} = do
   liftIO . withConnection dbfile $ \conn ->
-    execute conn "INSERT INTO users VALUES (?, ?, ?, date(?))" (name, idn, email, registrationDay)
+    execute conn "INSERT INTO users VALUES (?, ?, ?, date(?))"
+    (name, idn, email, registrationDay)
   return "usuario insertado exitosamente"
 
 handleNewMovie :: String -> Movie -> Handler String
 handleNewMovie dbfile Movie{..} = do
   liftIO . withConnection dbfile $ \conn ->
-    execute conn "INSERT INTO movies VALUES (?, ?, ?, ?, ?)" (title, idn, description, duration, rating)
+    execute conn "INSERT INTO movies VALUES (?, ?, ?, ?, ?)"
+    (title, idn, description, duration, rating)
   return "película insertada exitosamente"
 
-queryById' :: FromRow a => String -> Text -> Maybe Int -> Handler a
-queryById' _ _ Nothing = throwError err404 {errBody = "No se especificó ID"}
-queryById' db tbl (Just idn) = do
+queryById :: FromRow a => String -> Text -> Maybe Int -> Handler a
+queryById _ _ Nothing = throwError err404 {errBody = "No se especificó ID"}
+queryById db tbl (Just idn) = do
   result <- liftIO . withConnection db  $ \conn ->
     query conn ("SELECT * FROM " <> Query tbl <> " WHERE idn = ?") (Only idn)
   if not $ null result
@@ -69,10 +46,10 @@ database :: String
 database = "testdb.sqlite"
 
 userServer :: Server UserAPI
-userServer = queryById' database "users" :<|> handleNewUser database
+userServer = queryById database "users" :<|> handleNewUser database
 
 movieServer :: Server MovieAPI
-movieServer = queryById' database "movies" :<|> handleNewMovie database
+movieServer = queryById database "movies" :<|> handleNewMovie database
 
 appAPI :: Proxy AppAPI
 appAPI = Proxy
@@ -86,8 +63,14 @@ app2 = serve appAPI $ userServer :<|> movieServer
 -- inicializa la base de datos
 initDB :: String -> IO ()
 initDB db = withConnection db $ \conn -> do
-  execute conn "CREATE TABLE IF NOT EXISTS users (name TEXT, idn INTEGER PRIMARY KEY, email TEXT UNIQUE, registrationDay TEXT)" ()
-  execute conn "CREATE TABLE IF NOT EXISTS movies (title TEXT, idn INTEGER PRIMARY KEY, description TEXT, duration INTEGER, rating INTEGER)" ()
+  execute
+    conn ("CREATE TABLE IF NOT EXISTS users (name TEXT," <>
+           " idn INTEGER PRIMARY KEY, email TEXT UNIQUE," <>
+           "registrationDay TEXT)") ()
+  execute
+    conn ("CREATE TABLE IF NOT EXISTS movies (title TEXT," <>
+           " idn INTEGER PRIMARY KEY, description TEXT, " <>
+           "duration INTEGER, rating INTEGER)") ()
 
 -- |
 -- la rutina de IO que se va a ejecutar como servidor.
